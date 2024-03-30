b87a60ffb8aa287cbeb667c9f4d71200e8cc423a
ertugrullozer <ertugrulozer19944@gmail.com>
2024-03-30 13:49:16 +0300

    Ürünlerin Listelenmesi

--- Test_Product/Controllers/ProductController.cs
+++ Test_Product/Controllers/ProductController.cs
@@ -1,12 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Test_Product.Controllers
 {
     public class ProductController : Controller
     {
+        ProductManager productManager = new ProductManager(new EfProductDal());
         public IActionResult Index()
         {
-            return View();
+            var values = productManager.TGetList();
+            return View(values);
         }
     }
 }
