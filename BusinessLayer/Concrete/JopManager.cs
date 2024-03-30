14fc86f4e28de3d580986e286951d38027088eb7
ertugrullozer <ertugrulozer19944@gmail.com>
2024-03-31 02:50:21 +0300

    Jop Crud İşlemleri ve İkon Güncelleme işlemi yapıldı

---
+++ BusinessLayer/Concrete/JopManager.cs
@@ -0,0 +1,49 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using DataAccessLayer.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BusinessLayer.Concrete
+{
+    
+    public class JopManager : IJopService
+    {
+        IJopDal _jopDal;
+
+        public JopManager(IJopDal jopDal)
+        {
+            _jopDal = jopDal;
+        }
+
+        public void TDelete(Jop t)
+        {
+         _jopDal.Delete(t);
+
+        }
+
+        public Jop TGetById(int id)
+        {
+          return _jopDal.GetById(id);
+        }
+
+        public List<Jop> TGetList()
+        {
+            return _jopDal.GetList();
+        }
+
+        public void TInsert(Jop t)
+        {
+          _jopDal.Insert(t);
+        }
+
+        public void TUpdate(Jop t)
+        {
+            _jopDal.Updete(t);
+        }
+    }
+}
---
+++ Test_Product/Controllers/JopController.cs
@@ -0,0 +1,47 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Test_Product.Controllers
+{
+    public class JopController : Controller
+    {
+        JopManager jopManager=new JopManager(new EfJopDal());
+        public IActionResult Index()
+        {
+            var value = jopManager.TGetList();
+            return View(value);
+        }
+        public IActionResult JopDelete(int id) 
+        {
+            var value = jopManager.TGetById(id);
+            jopManager.TDelete(value);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult JopAdd() 
+        { 
+            return View(); 
+        }
+        [HttpPost]
+        public IActionResult JopAdd(Jop p)
+        {
+            jopManager.TInsert(p);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult JopUpdate(int id)
+        {
+            var value=jopManager.TGetById(id);
+            return View(value);
+        }
+        [HttpPost]
+        public IActionResult JopUpdate(Jop p)
+        {
+            jopManager.TUpdate(p);
+            return RedirectToAction("Index");
+        }
+    }
+}
