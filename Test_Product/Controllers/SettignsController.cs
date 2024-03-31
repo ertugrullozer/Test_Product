ee468d3a43349606102fafc66812a364207cad5d
ertugrullozer <ertugrulozer19944@gmail.com>
2024-03-31 11:09:02 +0300

    İdentity kkütüpjaneri kuruldu

---
+++ Test_Product/Controllers/SettignsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Test_Product.Controllers
+{
+    public class SettignsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
