e0a04191b91a0e4c82fb3ec6a88f5181f80e0c45
ertugrullozer <ertugrulozer19944@gmail.com>
2024-04-01 14:53:46 +0300

    Register View

--- Test_Product/Program.cs
+++ Test_Product/Program.cs
@@ -1,7 +1,14 @@
+using DataAccessLayer.Contcrete;
+using EntityLayer.Concrete;
+
 var builder = WebApplication.CreateBuilder(args);
 
+
 // Add services to the container.
+builder.Services.AddDbContext<Context>();
+builder.Services.AddIdentity<AppUser,AppRole>().AddEntityFrameworkStores<Context>();
 builder.Services.AddControllersWithViews();
+
 
 var app = builder.Build();
 
