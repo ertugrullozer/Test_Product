a38c68cb661007873b8762d09144d0f99a22ae2f
ertugrullozer <ertugrulozer19944@gmail.com>
2024-04-01 14:17:51 +0300

    Register View Modelin Oluşturulması

--- Test_Product/Controllers/RegisterController.cs
+++ Test_Product/Controllers/RegisterController.cs
@@ -1,12 +1,51 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Test_Product.Models;
 
 namespace Test_Product.Controllers
 {
     public class RegisterController : Controller
     {
+        private readonly UserManager<AppUser> _userManager;
+
+        public RegisterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(UserRegisterViewModel model)
+        {
+            AppUser appUser = new AppUser()
+            {
+                Name = model.Name,
+                Surname = model.SurName,
+                UserName = model.UserName,
+                Email=model.Mail
+
+            };
+            if (model.Password==model.ConfirmPassword) 
+            {
+                var result=await _userManager.CreateAsync(appUser);
+                if (result.Succeeded) 
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
+            }
+            return View(model);
         }
     }
 }
